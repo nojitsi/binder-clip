0a06be72f1cf471b79b2bc5f84c0504436675b02
yehoriliushchenko <yehor.iliuschenko@gen.tech>
2023-01-26 20:00:01 +0200

    0.9.9

---
+++ src/main.rs
@@ -0,0 +1,231 @@
+use std::process::exit;
+use imgui::{Condition, InputTextCallbackHandler, InputTextMultilineCallback, StyleColor, TextCallbackData};
+use textwrap::{LineEnding, Options, WordSeparator, WordSplitter, WrapAlgorithm};
+
+use crate::support::{MAX_FONT_SIZE, MIN_FONT_SIZE, Mod};
+
+mod support;
+
+pub const BUTTON_FONT_SIZE: i32 = 13;
+
+#[derive(Copy, Clone)]
+struct InputTextProps {
+    pub input_font_size: i32,
+    pub input_width: i32,
+    pub should_redraw: bool,
+}
+
+fn main() {
+    let Mod { system, font_id_hash_map, mut window_props } = support::init("Binder-Clip");
+    // let mut input_text = String::from("What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Why do we use it?It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).");
+    let mut input_text = String::from("");
+    let mut input_font_size: i32 = system.font_size as i32;
+
+    let mut input_text_props = InputTextProps {
+        input_font_size,
+        input_width: 0,
+        should_redraw: false
+    };
+
+    const WRAP_TEXT_CONTENT: fn(String, i32, i32) -> String = |content: String, display_width_px: i32, font_size_px: i32| {
+        let max_chars_in_line = display_width_px / (font_size_px / 2);
+
+        let mut pure_text = content.to_string();
+
+        let mut last_char_is_r = false;
+        pure_text.retain(|char| {
+            if char == '\r' {
+                last_char_is_r = true;
+                return false;
+            }
+
+            if char == '\n' && last_char_is_r {
+                last_char_is_r = false;
+                return false;
+            }
+
+            last_char_is_r = false;
+
+            return true;
+        });
+
+        let lines = pure_text.split("\n");
+        let mut wrapped_text_lines = vec![];
+
+        {
+            for content_line in lines {
+                let chars_in_line = content_line.chars().count();
+
+                if chars_in_line <= max_chars_in_line as usize {
+                    wrapped_text_lines.push(content_line.to_string());
+                    continue;
+                }
+
+                let mut wrapped_line = String::from(content_line);
+
+                wrapped_line = wrapped_line.replace(" ", "  ");
+                textwrap::fill_inplace(&mut wrapped_line, max_chars_in_line as usize);
+                wrapped_line = wrapped_line.replace("  ", " ");
+
+                let wrapped_lines = wrapped_line.split("\n");
+
+                let mut index = 0;
+                let number_of_wrapped_lines = wrapped_lines.clone().count();
+
+                let lines_with_rseperator = wrapped_lines.map(|line| {
+
+                    let end_char;
+                    if number_of_wrapped_lines == index + 1
+                    { end_char = "" } else { end_char = "\r" } ;
+
+                    index += 1;
+
+                    return line.to_string() + end_char;
+                });
+
+                wrapped_text_lines.extend(lines_with_rseperator);
+            }
+        }
+
+        return String::from(wrapped_text_lines.join("\n"));
+    };
+
+    let update_font_size = |input_text_props: &mut InputTextProps, input_text: &mut String, change: i32| {
+        let size = input_text_props.input_font_size.clone() + change;
+        if size < MIN_FONT_SIZE || size > MAX_FONT_SIZE {
+            return;
+        }
+        input_text_props.input_font_size = size.clone();
+
+        let wrapped_content = WRAP_TEXT_CONTENT(
+            String::from(input_text.clone()),
+            input_text_props.input_width,
+            input_text_props.input_font_size,
+        );
+
+        *input_text = wrapped_content;
+    };
+
+    struct InputTextEditCallbackHandler {
+        input_text_props: InputTextProps
+    }
+    impl InputTextCallbackHandler for InputTextEditCallbackHandler {
+        fn on_always(&mut self, mut data: TextCallbackData) {
+            let cursor_position = data.cursor_pos();
+
+            if self.input_text_props.should_redraw {
+                let input_content = String::from(data.str());
+
+                let wrapped_string: &String = &WRAP_TEXT_CONTENT(
+                    input_content,
+                    self.input_text_props.input_width,
+                    self.input_text_props.input_font_size,
+                );
+
+                data.clear();
+                data.insert_chars(0, wrapped_string);
+            }
+
+            data.set_cursor_pos(cursor_position);
+        }
+    }
+
+    let mut input_width = 0;
+    let mut before_input_text = input_text.clone();
+
+    system.main_loop(move |_, ui| {
+        let font_id_hash_map = &font_id_hash_map;
+
+        let new_input_width = (ui.io().display_size[0] - 21.0) as i32;
+
+        input_text_props.input_width = new_input_width.clone();
+        //wrap text on resize
+        input_text_props.should_redraw = (input_width != new_input_width) || (before_input_text != input_text);
+        let input_text_edit_handler = InputTextEditCallbackHandler { input_text_props };
+
+        input_width = new_input_width;
+
+        const HALF_BLACK_FRAME: [f32; 4] = [0.03, 0.00, 0.01, 0.5];
+        const HALF_BLACK_BTN: [f32; 4] = [0.13, 0.1, 0.10, 0.5];
+        const HALF_GREEN_BTN_HOVER: [f32; 4] = [0.0, 0.7, 0.0, 0.3];
+        const HALF_GREEN: [f32; 4] = [0.0, 0.7, 0.0, 0.7];
+        let frame_bg = ui.push_style_color(StyleColor::FrameBg, HALF_BLACK_FRAME);
+        let window_bg =  ui.push_style_color(StyleColor::WindowBg, HALF_BLACK_FRAME);
+
+        let button_bg = ui.push_style_color(StyleColor::Button, HALF_BLACK_BTN);
+        let button_hover_bg = ui.push_style_color(StyleColor::ButtonHovered, HALF_GREEN_BTN_HOVER);
+        let button_active_bg = ui.push_style_color(StyleColor::ButtonActive, HALF_GREEN);
+        let text_selection_bg = ui.push_style_color(StyleColor::TextSelectedBg, HALF_GREEN_BTN_HOVER);
+
+        ui
+            .window("main")
+            // .draw_background(true)
+            .size(ui.io().display_size, Condition::Always)
+            .position([0.0, 0.0],Condition::FirstUseEver)
+            .scroll_bar(true)
+            // .always_auto_resize(true)
+            .always_use_window_padding(true)
+            .resizable(false)
+            .title_bar(false)
+            .no_decoration()
+            .bg_alpha(0.5)
+            .build(|| {
+                before_input_text = input_text.to_string();
+
+                ui.separator();
+
+                let font_override = ui.push_font(*font_id_hash_map.get(&input_text_props.input_font_size).unwrap());
+
+                ui.input_text_multiline("##multiline", &mut input_text, [ui.io().display_size[0] - 16.0, ui.io().display_size[1] - 48.0])
+                    .allow_tab_input(true)
+                    .no_horizontal_scroll(true)
+                    .callback(InputTextMultilineCallback::ALWAYS | InputTextMultilineCallback::EDIT, input_text_edit_handler)
+                    .build();
+
+                font_override.end();
+                ui.separator();
+
+                ui.text(format!("Font size: {}", input_text_props.input_font_size));
+
+                let button_font_override = ui.push_font(*font_id_hash_map.get(&BUTTON_FONT_SIZE).unwrap());
+
+                let button_repeat_override = ui.push_button_repeat(true);
+                ui.same_line_with_spacing(135.0, 0.0);
+                if ui.button_with_size("+", [25.0, 20.0]) {
+                    update_font_size(&mut input_text_props, &mut input_text, 1);
+                };
+
+                ui.same_line_with_spacing(165.0,0.0);
+                if ui.button_with_size("-", [25.0, 20.0]) {
+                    update_font_size(&mut input_text_props, &mut input_text, -1);
+                };
+                button_repeat_override.end();
+
+                let always_on_top_button_color;
+                if window_props.always_on_top {
+                    always_on_top_button_color = HALF_GREEN_BTN_HOVER;
+                }
+                else {
+                    always_on_top_button_color = HALF_BLACK_FRAME;
+                }
+
+                let color_override = ui.push_style_color(StyleColor::Button, always_on_top_button_color);
+
+                ui.same_line_with_pos(ui.io().display_size[0] - 183.0);
+                if ui.button_with_size("Window always on Top", [145.0, 20.0]) {
+                    window_props.always_on_top = !window_props.always_on_top;
+                };
+
+                color_override.end();
+
+                ui.same_line_with_pos(ui.io().display_size[0] - 33.0);
+                if ui.button_with_size("X", [25.0, 20.0]) {
+                    exit(0x0100);
+                };
+
+                button_font_override.end();
+            });
+
+        return window_props;
+    },);
+}
---
+++ src/support/mod.rs
@@ -0,0 +1,272 @@
+use std::collections::HashMap;
+use std::path::Path;
+use glium::glutin;
+use glium::glutin::event::{Event, WindowEvent};
+use glium::glutin::event_loop::{ControlFlow, EventLoop};
+
+use glium::{Display, Surface};
+use imgui::{Context, FontConfig, FontGlyphRanges, FontId, FontSource, Ui};
+use imgui_glium_renderer::Renderer;
+use imgui_winit_support::{HiDpiMode, WinitPlatform};
+use std::time::Instant;
+use winit::dpi::PhysicalSize;
+use winit::platform::macos::WindowBuilderExtMacOS;
+use winit::window::Icon;
+
+mod clipboard;
+
+pub const MIN_FONT_SIZE: i32 = 5;
+pub const MAX_FONT_SIZE: i32 = 44;
+
+const SYSTEM_FONT_SIZE: f32 = 18.0;
+
+const DEFAULT_ALWAYS_ON_TOP: bool = true;
+
+fn load_icon(path: &Path) -> Icon {
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open(path)
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    return Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
+}
+
+pub struct System {
+    pub event_loop: EventLoop<()>,
+    pub imgui: Context,
+    pub platform: WinitPlatform,
+    pub renderer: Renderer,
+    pub font_size: f32,
+    pub display: glium::Display,
+}
+
+#[derive(Copy, Clone)]
+pub struct WindowProps {
+    pub always_on_top: bool
+}
+
+pub struct Mod {
+    pub system: System,
+    pub font_id_hash_map: HashMap<i32, FontId>,
+    pub window_props: WindowProps,
+}
+
+pub fn init(title: &str) -> Mod {
+    let window_props = WindowProps {
+        always_on_top: DEFAULT_ALWAYS_ON_TOP
+    };
+
+    let event_loop = EventLoop::new();
+    let context = glutin::ContextBuilder::new()
+        .with_vsync(false)
+        .with_depth_buffer(24)
+        .with_hardware_acceleration(Option::from(true))
+        .with_multisampling(0)
+        // .with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGl, (3, 3)))
+        .with_double_buffer(Option::from(true));
+
+    let builder = glutin::window::WindowBuilder::new()
+        .with_title(title)
+        .with_movable_by_window_background(true)
+        .with_titlebar_hidden(true)
+        .with_always_on_top(window_props.always_on_top)
+        .with_decorations(false)
+        .with_min_inner_size(PhysicalSize::new(760, 500))
+        .with_fullsize_content_view(true)
+        .with_transparent(true)
+        .with_has_shadow(false);
+
+    let display =
+        Display::new(builder, context, &event_loop).expect("Failed to initialize display");
+
+    let mut imgui = Context::create();
+    imgui.set_ini_filename(None);
+
+    if let Some(backend) = clipboard::init() {
+        imgui.set_clipboard_backend(backend);
+    } else {
+        eprintln!("Failed to initialize clipboard");
+    }
+
+    let mut platform = WinitPlatform::init(&mut imgui);
+    {
+        let gl_window = display.gl_window();
+        let window = gl_window.window();
+
+        let dpi_mode = if let Ok(factor) = std::env::var("IMGUI_EXAMPLE_FORCE_DPI_FACTOR") {
+            // Allow forcing of HiDPI factor for debugging purposes
+            match factor.parse::<f64>() {
+                Ok(f) => HiDpiMode::Locked(f),
+                Err(e) => panic!("Invalid scaling factor: {}", e),
+            }
+        } else {
+            HiDpiMode::Default
+        };
+
+        platform.attach_window(imgui.io_mut(), window, dpi_mode);
+    }
+
+    // Fixed font size. Note imgui_winit_support uses "logical
+    // pixels", which are physical pixels scaled by the devices
+    // scaling factor. Meaning, 13.0 pixels should look the same size
+    // on two different screens, and thus we do not need to scale this
+    // value (as the scaling is handled by winit)
+    let font_size = SYSTEM_FONT_SIZE;
+
+    imgui.fonts().add_font(&[
+        FontSource::TtfData {
+            data: include_bytes!("../../resources/fonts/ubuntu-mono.ttf"),
+            size_pixels: font_size,
+            config: Some(FontConfig {
+                // As imgui-glium-renderer isn't gamma-correct with
+                // it's font rendering, we apply an arbitrary
+                // multiplier to make the font a bit "heavier". With
+                // default imgui-glow-renderer this is unnecessary.
+                rasterizer_multiply: 1.5,
+                // Oversampling font helps improve text rendering at
+                // expense of larger font atlas texture.
+                oversample_h: 4,
+                oversample_v: 4,
+                ..FontConfig::default()
+            }),
+        },
+    ]);
+
+    let mut font_id_hash_map = HashMap::new();
+    let font_atlas = imgui.fonts();
+    for font_size in MIN_FONT_SIZE..=MAX_FONT_SIZE {
+        let font_id = font_atlas.add_font(&[
+            FontSource::TtfData {
+                data: include_bytes!("../../resources/fonts/ubuntu-mono.ttf"),
+                size_pixels: font_size as f32,
+                config: Some(FontConfig {
+                    rasterizer_multiply: 1.5,
+                    oversample_h: 4,
+                    oversample_v: 4,
+                    ..FontConfig::default()
+                }),
+            },
+            FontSource::TtfData {
+                data: include_bytes!("../../resources/fonts/ubuntu-mono.ttf"),
+                size_pixels: font_size as f32,
+                config: Some(FontConfig {
+                    rasterizer_multiply: 1.5,
+                    oversample_h: 4,
+                    oversample_v: 4,
+                    glyph_ranges: FontGlyphRanges::cyrillic(),
+                    ..FontConfig::default()
+                }),
+            },
+        ]);
+        font_id_hash_map.insert(font_size, font_id);
+    }
+
+    let renderer = Renderer::init(&mut imgui, &display).expect("Failed to initialize renderer");
+
+    Mod {
+        system: System {
+            event_loop,
+            imgui,
+            platform,
+            renderer,
+            font_size,
+            display,
+        },
+        font_id_hash_map,
+        window_props,
+    }
+}
+
+impl System {
+    pub fn main_loop<F: FnMut(&mut bool, &mut Ui) -> WindowProps + 'static>(self, mut run_ui: F) {
+        let System {
+            event_loop,
+            mut imgui,
+            mut platform,
+            mut renderer,
+            display,
+            ..
+        } = self;
+        let mut last_frame = Instant::now();
+
+        event_loop.run(move |event, _, control_flow| match event {
+            Event::NewEvents(_) => {
+                let now = Instant::now();
+                imgui.io_mut().update_delta_time(now - last_frame);
+                last_frame = now;
+            }
+            Event::MainEventsCleared => {
+                let gl_window = display.gl_window();
+                platform
+                    .prepare_frame(imgui.io_mut(), gl_window.window())
+                    .expect("Failed to prepare frame");
+                gl_window.window().request_redraw();
+            }
+            Event::RedrawRequested(_) => {
+                let ui = imgui.frame();
+                let gl_window = display.gl_window();
+                let mut winit_window = gl_window.window();
+
+                // if ui.is_any_item_focused() {
+                //     println!("item in focus")
+                // }
+                //
+                // if ui.is_item_activated() {
+                //     // println!("item in activated")
+                // }
+                //
+                // if ui.is_item_clicked() {
+                //     // println!("item in clicked")
+                // }
+                //
+                // if ui.is_item_active() {
+                //     // println!("item in active")
+                // }
+
+                // if ui.is_any_item_hovered() {
+                //     // system_window.
+                //     // println!("item is activfe");
+                // }
+
+                let mut run = true;
+                let window_props: WindowProps = run_ui(&mut run, ui);
+                winit_window.set_always_on_top(window_props.always_on_top);
+
+                if !run {
+                    *control_flow = ControlFlow::Exit;
+                }
+
+
+
+                let mut target = display.draw();
+
+                target.clear_color_srgb(0.0, 0.0, 0.0, 0.7);
+                //
+                // target.clear_depth(5.0); // target.finish();
+                platform.prepare_render(ui, gl_window.window());
+
+                let draw_data = imgui.render();
+
+                // target.clear_all_srgb((0.0, 0.0, 0.0, 0.0), 1.0, 1);
+
+                renderer
+                    .render(&mut target, draw_data)
+                    .expect("Rendering failed");
+
+                target.finish().expect("Failed to swap buffers");
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            event => {
+                let gl_window = display.gl_window();
+                platform.handle_event(imgui.io_mut(), gl_window.window(), &event);
+            }
+        })
+    }
+}
+
